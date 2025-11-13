5f4c6c919c4b328c71b8a130e43e87c5d62209c0
abduaslam <abduaslam098@gmail.com>
2025-11-13 11:35:42 +0200

    update my portfolio

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 
+   const textElement = document.getElementById("typing-text");
+    const roles = ["Full Stack Developer", "Data analyst"];
+    let roleIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
 
+    function typeEffect() {
+      const current = roles[roleIndex];
+      const displayed = current.substring(0, charIndex);
+      textElement.textContent = `I'm a ${displayed}`;
+
+      if (!isDeleting && charIndex < current.length) {
+        charIndex++;
+        setTimeout(typeEffect, 100);
+      } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(typeEffect, 50);
+      } else {
+        isDeleting = !isDeleting;
+        if (!isDeleting) roleIndex = (roleIndex + 1) % roles.length;
+        setTimeout(typeEffect, 1000);
+      }
+    }
+
+    // Start typing once page loads
+    document.addEventListener("DOMContentLoaded", typeEffect);
 
